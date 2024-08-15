eb17b819917b2aa08b760463c9f390f308d71ea4
ulicesmora <ulicesyarit@gmail.com>
2024-08-15 12:19:20 -0600

    interfaz de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'registro', component: RegistroComponent },
 ];
